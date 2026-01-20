15955d038724e2739b002784fcbff71852b5fa64
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-01-20 20:05:54 +1100

    Feat: Added some info to about and projects content section

--- src/content/About/About.tsx
+++ src/content/About/About.tsx
@@ -1,23 +1,17 @@
-const about = [
-    'Penultimate computer science @ UNSW',
-    'Self-taught Python',
-    'Actively learning web development with React + Tailwind CSS + more',
-    'Extensive volunteering experience in high school + university'
-]
+import { NavLink } from "react-router-dom";
 
 export default function About() {
     return (
-        <div className="w-full h-screen p-20">
-            <h1>About</h1>
-
-            
-            <hr />
-            
-            <ul>
-                {about.map((item) => (
-                    <li>{item}</li>
-                ))}
-            </ul>
+        <div className="w-full h-screen p-40">
+            <h1 className="p-4">Hi!</h1>
+            <hr className="" />
+            <div className="p-4 text-xl">
+                <p>I'm Christopher, currently living in Sydney as a penultimate computer science @ UNSW.</p>
+                <p>Despite the lack of employability and experience, I am somewhat building projects to develop technical skills and keep up-to-date with the latest tech trends!</p>
+                <br />
+                <p>Besides creating personal projects for self-growth, I like to design and tinker with 3D models on Blender. You can view some old projects I created <NavLink to={'/projects'}>here!</NavLink></p>
+                <p>I'm just an average gacha gamer. Currently, </p>
+            </div>
         </div>
     )
 }
--- src/content/Projects/Projects.tsx
+++ src/content/Projects/Projects.tsx
@@ -1,20 +1,46 @@
-const projects = [
+interface Project {
+    title: string;
+    description: string;
+    tech: string[];
+    github: string;
+    app?: string;
+    status: string;
+}
+
+const projects: Project[] = [
     {
         title: 'Remly',
         description: 'A centralised web application for hosting group hangouts and major events with the add-on features of gambling stakes and community engagement.',
         tech: ['React', 'TypeScript', 'Tailwind CSS'],
-        github: 'https://github.com/devsoc-unsw/trainee-ratatouille-25t3'
-    },
-    {
+        github: 'https://github.com/devsoc-unsw/trainee-ratatouille-25t3',
+        status: 'Coming Soon...'
+    }, {
         title: 'Sortify',
-        description: ''
+        description: `
+        Automatically sort a large, messed-up Spotify playlist into one or more playlists split based on a designated criteria. 
+        This project is a web application that integrates with the Spotify API, allowing users to interact with their Spotify library through a Flask backend and a React frontend.
+        `,
+        tech: ['Python', 'Flask'],
+        github: 'https://github.com/Rushik-Rsenal/Hackathon',
+        status: 'Coming Soon...'
+    }, {
+        title: 'Felis Catus',
+        description: `
+        A web application created with Python + Flask for my the HSC subject: Information Processes & Technology (IPT).
+        Extracts and displays information via The Cat API.
+        Challenged myself to learn Flask and learn how to use an API within 3-4 weeks.
+        `,
+        tech: ['Python', 'Flask', 'HTML', 'CSS'],
+        github: 'https://github.com/CrispyKhim/Felis-Catus',
+        app: 'https://felis-catus.vercel.app/',
+        status: 'Archived'
     }
 ]
 
 
 export default function Projects() {
     return (
-        <div className="h-screen w-full max-w-6xl mx-auto px-4">
+        <div className="h-screen w-full px-40">
             <h1>Projects</h1>
             <div className="grid grid-cols-1 md:grid-cols gap-6">
                 {projects.map((item) => (
