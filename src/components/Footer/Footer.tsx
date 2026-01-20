63d4f9e77638b988f893a6735e72a7f0e90d7f91
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-01-21 10:37:31 +1100

    Feat: Updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <div className="p-4 flex justify-between bg-gray-500 px-50">
                 <p>© 2026 Christopher Khim</p>
-                <p>Disclaimer: I'm in the midst of learning React and flexbox, so expect some wacky bugs</p>
+                <p>Disclaimer: This project is still under-development; it won't all be black and white</p>
             </div>
         </>
     )
